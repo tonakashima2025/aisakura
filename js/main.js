286608e61064a0f9319e74d568dfab0f7ba3addb
tonakashima2025 <tonakashima2025@gmail.com>
2020-02-28 17:20:13 +0900

    20200228

--- js/main.js
+++ js/main.js
@@ -35,23 +35,7 @@ jQuery(function() {
   jQuery('#pagetop').click(function () {
     jQuery('html,body').animate({'scrollTop':0},'slow');
   });
-  
-  // naviScroll
-  // $('header a, footer a').click(function() {
-  //   var headerHight = 77;
-  //   var id = $(this).attr('href');
-  //   var position = $(id).offset().top;
-  //   $('html,body').animate({
-  //     'scrollTop':position - headerHight
-  //     },'slow');
-  //     return false;
-  // });
-  
-  // naviClick
-  // $('header li a').click(function() {
-  //   $('header li a').removeClass('active');
-  //   $(this).addClass('active');
-  // });
+
 });
 
 }
